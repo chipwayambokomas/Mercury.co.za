880b95e60ae6e7c374ef04f8a7f99eabdf0fef5a
Saul Chipwayambokoma <chipwayambokomas@gmail.com>
2023-01-05 16:50:11 +0200

    Finished Blog page

--- src/components/BlogSideSection.js
+++ src/components/BlogSideSection.js
@@ -1,9 +1,63 @@
 import './BlogSideSection.css'
 import React from 'react'
+import { LatestData, CategoryData } from './BlogSideSectionData'
 
 function BlogSideSection() {
   return (
-    <div>
+    <div className='BlogSideSection'>
+
+        <input type='text' placeholder='Search here...'></input>
+        <i className="fa-solid fa-magnifying-glass"></i>
+
+        <div className='Latest1'>
+            <h2>Latest Post</h2>
+
+            {LatestData.map((item,index) => {
+                return(
+            <div className='posts' key={index}>
+            <img alt='latestpic' className='latestpic' src={item.image}></img>
+            <p>{item.preview}</p>
+            </div>
+            )
+        })}
+
+        </div>
+
+        <div className='Categories'>
+
+            <h2>Categories</h2>
+
+            {CategoryData.map((item,index) => {  
+                return(
+
+            <div className='Category' key={index}>
+                <p className='categoryname'>{item.title}</p>
+                <button className='categorybutton'>{item.number}</button>
+            </div>
+            )
+        
+        })}
+        </div>
+
+        <div className='Tags'>
+
+            <h2>Tags</h2>
+
+            <div className='tags1'>
+                <button className='tagname1'>Roadways</button>
+                <button className='tagname2'>Airways</button>
+            </div>
+            <div className='tags1'>
+                <button className='tagname1'>Shipment</button>
+                <button className='tagname2'>Lorem Ipsum</button>
+            </div>
+            <div className='tags1'>
+                <button className='tagname1'>Lorem Ipsum</button>
+                <button className='tagname2'>Lorem Ipsum</button>
+            </div>
+        </div>
+
+
       
     </div>
   )
---
+++ src/components/BlogSideSectionData.js
@@ -0,0 +1,53 @@
+import pic1 from '../assests/latest1.png'
+import pic2 from '../assests/latest2.png'
+import pic3 from '../assests/latest3.png'
+import pic4 from '../assests/latest4.png'
+import pic5 from '../assests/latest5.png'
+
+export const LatestData = [
+    {
+        image:pic1,
+        preview: 'Minimal Post With A Preview Image',
+    },
+    {
+        image:pic2,
+        preview: 'Good Design Makes Me Happy',
+    },
+    {
+        image:pic3,
+        preview: 'Best 27 Design Blogs for 2021',
+    },
+    {
+        image:pic4,
+        preview: 'Creative Ways To Use Infographics For Your Business',
+    },
+    {
+        image:pic5,
+        preview:'Tumblr Banner Templates : Tips To Get Better Engagement',
+    },
+
+]
+
+export const CategoryData = [
+    {
+        title:'Shipment',
+        number: '05',
+    },
+    {
+        title:'Roadways',
+        number: '15',
+    },
+    {
+        title:'Airways',
+        number: '65',
+    },
+    {
+        title:'Lorem Ipsum',
+        number: '08',
+    },
+    {
+        title:'Lorem Ipsum',
+        number: '16',
+    },
+   
+]
